dde17bb7f772eb5f055be3f33d0108cc85535d2c
vicarb <vcardenasb@protonmail.com>
2024-03-04 10:15:19 -0300

    Initial Commit

---
+++ shopy/shopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopyApp.swift
+//  shopy
+//
+//  Created by Victor Cardenas Bahamonde on 04-03-24.
+//
+
+import SwiftUI
+
+@main
+struct shopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
